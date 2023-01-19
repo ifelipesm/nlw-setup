a2802757cde286691209c0fab0708b1a29d247f9
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-01-19 15:46:05 -0300

    Day 2 - Front-End (Header + Table)

--- Web/tailwind.config.cjs
+++ Web/tailwind.config.cjs
@@ -6,7 +6,15 @@ module.exports = {
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        background: '#09090A'
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+        },
+
+    },
   },
   plugins: [],
 }

b7f6e797e53f586a3934b1360bbdc6c65c80639f
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-01-20 20:01:30 -0300

    Day 3 - Mobile Components

--- Mobile/src/screens/Home.tsx
+++ Mobile/src/screens/Home.tsx
@@ -4,6 +4,7 @@ import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-yea
 
 import { DAY_SIZE, HabitDay } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { useNavigation } from '@react-navigation/native';
 
 const weekDays = ['D','S','T','Q','Q','S','S']
 
@@ -13,6 +14,8 @@ const amountOfDaysToFill= minimumSummaryDatesSizes - datesFromYearStart.length;
 
 
 export function Home(){
+
+  const { navigate } = useNavigation();
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header/>
@@ -40,6 +43,7 @@ export function Home(){
           datesFromYearStart.map(date => (
             <HabitDay
             key={date.toISOString()}
+            onPress={()=> navigate('habit',{date:date.toISOString()})}
             />
           ))
         }

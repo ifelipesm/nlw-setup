c05f68053468559632a0b133befbedbfa2147e3e
ifelipesm <55774080+ifelipesm@users.noreply.github.com>
2023-01-21 17:23:25 -0300

    Day 4 - Front-End: Checkbox + API Backend

--- Web/src/components/NewHabitForm.tsx
+++ Web/src/components/NewHabitForm.tsx
@@ -1,9 +1,58 @@
+import * as Checkbox from "@radix-ui/react-checkbox"
 import {Check} from "phosphor-react"
+import { FormEvent, useState } from "react"
+import { api } from "../lib/axios"
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-Feira',
+  'Terça-Feira',
+  'Quarta-Feira',
+  'Quinta-Feira',
+  'Sexta-Feira',
+  'Sábado'
+]
 
 export function NewHabitForm(){
+  const [title,setTitle] = useState('')
+  const [weekDays, setWeekDays] = useState<number[]>([])
+
+  async function  createNewHabit(event: FormEvent){
+    event.preventDefault()
+
+    if(!title || weekDays.length === 0){
+      return 
+    }
+    await api.post('habits',{
+      title,
+      weekDays,
+    })
+
+    setTitle('')
+    setWeekDays([])
+
+    alert('Hábito criado com sucesso')
+  }
+
+  //checked | unchecked
+  function handleToggleWeekDay(weekDay: number){
+    if(weekDays.includes(weekDay)){
+      //salvando a busca condicional na variável
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay)
+      //salvando o estado 
+      setWeekDays(weekDaysWithRemovedOne)
+    } else {
+      //guardando o array
+      const weekDaysWithAddedOne = [... weekDays, weekDay]
+      //salvando o array em um estado
+      setWeekDays(weekDaysWithAddedOne)
+    }
+
+  }
+
   return (
     
-    <form className="w-full flex flex-col mt-6">
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title"  className="font-semibold leading-tight" >
         Qual a sua meta?
       </label>
@@ -14,11 +63,42 @@ export function NewHabitForm(){
       placeholder="ex.: Exercícios, dormir bem, etc..."
       className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
       autoFocus
+      value={title}
+      onChange={event => setTitle(event.target.value)}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a sua disponibilidade?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map((weekDay,index) => {
+          return (
+            <Checkbox.Root 
+            key={weekDay} 
+            className="flex items-center gap-3 group"
+            checked={weekDays.includes(index)}
+            onCheckedChange={ () => handleToggleWeekDay(index) }
+            >
+            
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center
+               bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 
+               group-data-[state=checked]:border-green-500 "
+              >
+                <Checkbox.Indicator>
+                  <Check size={20}  className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span
+              className="text-white leading-tight"
+              >
+                {weekDay}
+              </span>
+            </Checkbox.Root>
+          )
+        })}
+        </div>
 
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500" >
       <Check size={20} weight="bold" />
@@ -28,5 +108,5 @@ export function NewHabitForm(){
     </form>
 
 
-  )
-}
+  )}
+  
